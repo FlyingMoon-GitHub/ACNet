0790b228cbe0d554794fc18a293d5aad3b7a697b
FlyingMoon-GitHub <MingyangYUE@hotmail.com>
2020-10-27 23:10:45 +0800

    Correct data augmentation method.

--- util/transformation.py
+++ util/transformation.py
@@ -70,9 +70,9 @@ def getTransformation(args, type):
     if type == 'train':
         transformation_list.append(transforms.RandomRotation(degrees=20))
         transformation_list.append(transforms.RandomResizedCrop(size=args.target_size, scale=(0.9, 1.0)))
-        transformation_list.append(transforms.ColorJitter(brightness=0.1))
+        # transformation_list.append(transforms.ColorJitter(brightness=0.1))
         transformation_list.append(transforms.RandomHorizontalFlip(p=0.5))
-        transformation_list.append(transforms.RandomVerticalFlip(p=0.5))
+        # transformation_list.append(transforms.RandomVerticalFlip(p=0.5))
 
     transformation_list.append(transforms.Resize((args.target_size, args.target_size)))
     transformation_list.append(transforms.ToTensor()),
