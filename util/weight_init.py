5ecc2281724af110788bffcec9c42e0c0c5c5f5c
FlyingMoon-GitHub <MingyangYUE@hotmail.com>
2020-10-28 11:22:03 +0800

    Correct weight initialization method.

--- util/weight_init.py
+++ util/weight_init.py
@@ -3,7 +3,12 @@
 from torch import nn
 
 def weightInit(module):
-    if isinstance(module, (nn.Conv2d, nn.Linear)):
+    if isinstance(module, nn.Conv2d):
+        if module.weight is not None:
+            nn.init.normal_(module.weight.data, 0, 0.05)
+        if module.bias is not None:
+            nn.init.constant_(module.bias.data, 0)
+    elif isinstance(module, nn.Linear):
         if module.weight is not None:
             nn.init.xavier_normal_(module.weight.data)
         if module.bias is not None:
