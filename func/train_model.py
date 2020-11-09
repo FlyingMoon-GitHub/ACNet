Correct weighting methods and loss functions.

func/train_model.py
@@ -58,10 +58,10 @@ def train(args, model, optimizers, learning_rate_schedulers, dataloaders):
 
                 leaves_out, final_out = model(image)
 
-                loss = nn.CrossEntropyLoss()(final_out, label)
+                loss = nn.NLLLoss()(final_out, label)
 
                 for out in leaves_out:
-                    loss = loss + nn.CrossEntropyLoss()(out, label)
+                    loss = loss + nn.NLLLoss()(out, label)
 
                 if args.use_cuda:
                     loss = loss.cuda()
@@ -97,6 +97,16 @@ def train(args, model, optimizers, learning_rate_schedulers, dataloaders):
                 val_acc = val_result['val_acc']
                 print('val_acc: {:6.4f}.'.format(val_acc))
 
+            if args.use_cuda:
+                torch.cuda.synchronize()
+
+            if (epoch + 1) % args.save_interval == 0:
+                save_path = os.path.join(args.save_dir, 'weight_stage' + str(1) + '_epoch_' + str(epoch) + '.pth')
+                torch.save(model.state_dict(), save_path)
+
+            if args.use_cuda:
+                torch.cuda.empty_cache()
+
     for epoch in range(args.start_epoch2, args.epoch_num2):
 
         cur_step = 0
@@ -122,10 +132,10 @@ def train(args, model, optimizers, learning_rate_schedulers, dataloaders):
 
             leaves_out, final_out = model(image)
 
-            loss = nn.CrossEntropyLoss()(final_out, label)
+            loss = nn.NLLLoss()(final_out, label)
 
             for out in leaves_out:
-                loss = loss + nn.CrossEntropyLoss()(out, label)
+                loss = loss + nn.NLLLoss()(out, label)
 
             if args.use_cuda:
                 loss = loss.cuda()
@@ -161,11 +171,11 @@ def train(args, model, optimizers, learning_rate_schedulers, dataloaders):
             val_acc = val_result['val_acc']
             print('val_acc: {:6.4f}.'.format(val_acc))
 
-        save_path = os.path.join(args.save_dir, 'weight_epoch_' + str(epoch) + '.pth')
-
         if args.use_cuda:
             torch.cuda.synchronize()
 
+        if (epoch + 1) % args.save_interval == 0:
+            save_path = os.path.join(args.save_dir, 'weight_stage' + str(2) + '_epoch_' + str(epoch) + '.pth')
         torch.save(model.state_dict(), save_path)
 
         if args.use_cuda:

model/binaryneuraltree.py
@@ -22,6 +22,7 @@ class BinaryNeuralTree(nn.Module):
         self.branch_routings = self._branch_routings()
         self.attention_transformers = self._attention_transformers()
         self.label_predictions = self._label_predictions()
+        self.logs = self._log()
 
     def _branch_routings(self):
         structure = [[None for _ in range(int(pow(2, i)))] for i in range(self.tree_height - 1)]
@@ -59,6 +60,16 @@ class BinaryNeuralTree(nn.Module):
 
         return structure
 
+    def _log(self, epsilon=1e-12):
+        structure = [None] * int(pow(2, self.tree_height - 1) + 1)
+        cur = 0
+        for i in range(int(pow(2, self.tree_height - 1)) + 1):
+            self.__setattr__('log' + str(cur), (lambda x: torch.log(x + epsilon)))
+            structure[i] = self.__getattribute__('log' + str(cur))
+            cur += 1
+
+        return structure
+
     def forward(self, x):
         probs = [[None for _ in range(int(pow(2, i + 1)))] for i in range(self.tree_height - 1)]
         features = [[None for _ in range(int(pow(2, i)))] for i in range(self.tree_height)]
@@ -77,11 +88,15 @@ class BinaryNeuralTree(nn.Module):
                 features[i + 1][j * 2] = self.attention_transformers[i][j * 2](features[i][j])
                 features[i + 1][j * 2 + 1] = self.attention_transformers[i][j * 2 + 1](features[i][j])
 
-        leaves_out = tuple((self.label_predictions[i](features[self.tree_height - 1][i]) for i in
-                            range(int(pow(2, self.tree_height - 1)))))
+        leaves_out = [self.label_predictions[i](features[self.tree_height - 1][i]) for i in
+                      range(int(pow(2, self.tree_height - 1)))]
 
         final_out = probs[self.tree_height - 2][0] * leaves_out[0]
         for i in range(1, int(pow(2, self.tree_height - 1))):
             final_out = final_out + probs[self.tree_height - 2][i] * leaves_out[i]
+
+        leaves_out = tuple((self.logs[i](leaves_out[i]) for i in range(int(pow(2, self.tree_height - 1)))))
+
+        final_out = self.logs[-1](final_out)
 
         return leaves_out, final_out

model/branchrouting.py
@@ -8,7 +8,7 @@ from model.gcblock import *
 
 
 class BranchRoutingModule(nn.Module):
-    def __init__(self, in_channels=512):
+    def __init__(self, in_channels=512, epsilon=1e-12):
         super(BranchRoutingModule, self).__init__()
 
         self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size=1)
@@ -17,7 +17,7 @@ class BranchRoutingModule(nn.Module):
         self.fc = nn.Linear(in_channels, 1)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.signedsqrt = lambda x: torch.sign(x) * torch.sqrt(torch.sign(x) * x + 1e-12)
+        self.signedsqrt = lambda x: torch.sign(x) * torch.sqrt(torch.sign(x) * x + epsilon)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):

util/arg_parse.py
@@ -19,6 +19,15 @@ def getArgs():
     parser.add_argument('--anno_dir', dest='anno_dir',
                         default='annos', type=str)
 
+    parser.add_argument('--savepoint_file', dest='savepoint_file',
+                        default=None, type=str)
+    parser.add_argument('--save_dir', dest='save_dir',
+                        default=os.path.join('.', 'checkpoint'), type=str)
+    parser.add_argument('--log_dir', dest='log_dir',
+                        default=os.path.join('.', 'log'), type=str)
+    parser.add_argument('--save_interval', dest='save_interval',
+                        default=5, type=int)
+
     parser.add_argument('--backbone', dest='backbone',
                         default='resnet50', type=str)
     parser.add_argument('--type', dest='type',
@@ -80,13 +89,6 @@ def getArgs():
 
     parser.add_argument('--use_cuda', dest='use_cuda',
                         default=torch.cuda.is_available(), type=bool)
-
-    parser.add_argument('--savepoint_file', dest='savepoint_file',
-                        default=None, type=str)
-    parser.add_argument('--save_dir', dest='save_dir',
-                        default=os.path.join('.', 'checkpoint'), type=str)
-    parser.add_argument('--log_dir', dest='log_dir',
-                        default=os.path.join('.', 'log'), type=str)
 
     # LACK OF TRANSFORMATION-RELATED ARGUMENTS
 